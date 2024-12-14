e019c93ad4a37144b9f44c9180a9f04930737b15
Capitan77 <dael202106@gmail.com>
2024-12-14 12:56:27 -0500

    S6

--- src/main/java/com/cibertec/servlets/ListadoUsuariosServlets.java
+++ src/main/java/com/cibertec/servlets/ListadoUsuariosServlets.java
@@ -1,6 +1,33 @@
 package com.cibertec.servlets;
 
+import com.cibertec.dao.UsuarioDao;
+import com.cibertec.dao.impl.UsuarioDaoImpl;
+import com.cibertec.models.Usuario;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class ListadoUsuariosServlets extends HttpServlet {
+
+    UsuarioDao usuarioDao = new UsuarioDaoImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+
+            List<Usuario> usuarios = usuarioDao.obtenerUsuarios();
+            req.setAttribute("usuarios", usuarios);
+            req.getRequestDispatcher("usuarios.jsp").forward(req, resp);
+
+        }catch (SQLException error) {
+            throw  new ServletException("Error al listar los usuarios", error);
+        }
+
+    }
 }
---
+++ src/main/webapp/usuarios.jsp
@@ -0,0 +1,37 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+     <div class="container mt-5">
+
+     <table class="table table-bordered table-hover">
+             <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Apellido</th>
+                      <th>Correo</th>
+                  </tr>
+             </thead>
+             <tbody>
+             <c:forEach var="usu" items="${usuarios}">
+             <tr>
+             <td>${usu.id}</td>
+             <td>${usu.nombre}</td>
+             <td>${usu.apellido}</td>
+             <td>${usu.correo}</td>
+             </tr>
+             </c:forEach>
+             </tbody>
+     </table>
+
+     </div>
+
+
+</body>
+</html>
